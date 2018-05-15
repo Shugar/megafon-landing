2c8408fe74cff2cfab95ce2824973250d73d2ebd
vanilla-ice <valeraerohin97@mail.ru>
2018-05-15 19:35:55 +0300

    Feature: slider

--- app/initialize.js
+++ app/initialize.js
@@ -1,5 +1,17 @@
 import $ from 'jquery'
+import 'slick-carousel'
 
 $(document).ready(() => {
-  console.log('Ready!')
+  const slidesCount = $('.slider').find('.slider__slide').length
+  $('.js-amount').html(`${slidesCount}`)
+
+  $('.slider').slick({
+    slidesToShow: 3,
+    infinite: true,
+    arrows: true
+  })
+
+  $('.slider').on('beforeChange', (e, slick, curr, next) => {
+    $('.js-curr').html(`${next + 1}`)
+  })
 })
